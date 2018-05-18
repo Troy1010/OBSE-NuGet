66e9c0186ec52c6dbc381cbe9fc4e9e5d5ac3384
Unknown <drmcmurderface2@mail.com>
2018-05-18 01:00:01 -0500

    Package now installs obse/* and common/*

--- packing/conanfile.py
+++ packing/conanfile.py
@@ -1,41 +1,29 @@
 from conans import ConanFile, CMake, tools
-
+import subprocess, os, errno
 
 class OBSEPackaging(ConanFile):
-    name = "OBSE Packaging"
+    name = "OBSE_Packaging"
     version = "0.1"
     license = "MIT"
     url = "https://github.com/Troy1010/OBSE-Packaging"
     description = "Packages OBSE as a Conan package."
 
     def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
-        self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-                              '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        try:
+            os.makedirs('Oblivion-Script-Extender')
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+        os.chdir('Oblivion-Script-Extender')
+        subprocess.call(['git', 'clone', 'https://github.com/llde/Oblivion-Script-Extender.git', '.', '--no-checkout'])
+        subprocess.call(['git', 'checkout', '338206760744df35711bde343d7efe5367644d75'])
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="hello")
-        cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
+        pass
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        self.copy('common/*',src='Oblivion-Script-Extender')
+        self.copy('obse/*',src='Oblivion-Script-Extender')
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        pass
