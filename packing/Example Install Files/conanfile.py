2423766e332b666bae7ccc0cb9606527c7b0bb22
Unknown <drmcmurderface2@mail.com>
2018-05-29 09:37:04 -0500

    small refactor

--- packing/Example Install Files/conanfile.py
+++ packing/Example Install Files/conanfile.py
@@ -4,5 +4,5 @@ class TM_HelloWorld_Conan(ConanFile):
     requires="OBSEPluginDevPackage/0.1@Troy1010/channel"
     generators="visual_studio"
 
-    import HookBuildInfo as HBI
-    HBI.HookBuildInfo('PROJECT.vcxproj','conanbuildinfo.props')
+    import BuildInfoIntegration as BII
+    BII.HookBuildInfo('PROJECT.vcxproj','conanbuildinfo.props')
