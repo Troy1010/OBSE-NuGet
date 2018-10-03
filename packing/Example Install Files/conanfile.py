08b45000c4b7b224b4fbc5ebf0a843f2c003a30b
Unknown <drmcmurderface2@mail.com>
2018-10-03 01:21:01 -0500

    Example Install Files

--- packing/Example Install Files/conanfile.py
+++ packing/Example Install Files/conanfile.py
@@ -1,29 +1,33 @@
+##region Settings
+bPause = True
+sProj = "CompleteControl/CompleteControl.vcxproj"
+sSln = "CompleteControl.sln"
+##endregion
+##region Imports
 from conans import ConanFile
-import xml.etree.ElementTree
-import os, sys, ctypes
-import VisualStudioAutomation as VS
+import os, sys
 import TM_CommonPy as TM
-
+import traceback
+##endregion
+##region DoubleclickEvent
 if __name__ == "__main__":
     try:
-        #---Open
-        sProj = os.path.join("HelloWorld","HelloWorld.vcxproj")
-        #---
         TM.Run("conan install . -pr conanprofile_OBSEPlugin")
-        VS.SetTMDefaultSettings(sProj)
-        VS.IntegrateProps(sProj,"conanbuildinfo.props")
-        ##region Integrate OBSEPluginDevPackage RecommendedIntegrationFiles
-        with TM.ElementTreeContext("conanbuildinfo.props") as vTree:
-            sOBSEPluginDevPackageRoot = TM.FindElem(xml.etree.ElementTree.Element("Conan-OBSEPluginDevPackage-Root"),vTree).text
-        sys.path.insert(0,sOBSEPluginDevPackageRoot)
-        import RecommendedIntegration
-        RecommendedIntegration.Main(os.path.abspath(sProj),sOBSEPluginDevPackageRoot)
-        ##endregion
-    except:
-        ctypes.windll.user32.MessageBoxW(0, "Exception occured.", "conanfile.py", 1)
-        raise
+    except Exception as e:
+        print("====================================================================")
+        print("Traceback (most recent call last):")
+        traceback.print_tb(e.__traceback__)
+        print(e)
+        os.system('pause')
+        sys.exit(1)
+    if bPause:
+        print("\n\t\t\tDone\n")
+        os.system('pause')
+##endregion
 
-class TM_HelloWorld_Conan(ConanFile):
-    name="HelloWorld"
-    requires="OBSEPluginDevPackage/0.1@Troy1010/channel"
-    generators = "visual_studio"
+class CompleteControl_Conan(ConanFile):
+    name="CompleteControl"
+    requires="OBSEPluginDevPackage/0.1@Troy1010/channel","TM_CommonCPP/0.1@Troy1010/channel"
+    generators="visual_studio"
+    license = "MIT"
+    description = "OBSE plugin to resolve EnableKey conflicts between mods."
