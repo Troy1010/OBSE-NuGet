e4a40db876a912b5fdca4b61cd14189d42854f64
Unknown <drmcmurderface2@mail.com>
2018-10-03 19:49:40 -0500

    SetIncludeDir, Finally Working

--- packing/conanfile.py
+++ packing/conanfile.py
@@ -1,22 +1,9 @@
 ##region Settings
 bPause = True
 ##endregion
-from conans import ConanFile, MSBuild
-import subprocess, os, errno, ctypes
-import TM_CommonPy as TM
-
-##region DoubleclickEvent
-if __name__ == "__main__":
-    try:
-        TM.Run("conan create . Troy1010/channel -pr conanprofile_OBSEPlugin")
-    except Exception as e:
-        print(e)
-        os.system('pause')
-        raise
-    if bPause:
-        print("\n\t\t\tDone\n")
-        os.system('pause')
-##endregion
+from conans import ConanFile
+import subprocess, os
+import VisualStudioAutomation as VS
 
 class OBSEPackaging_Conan(ConanFile):
     name = "OBSEPluginDevPackage"
@@ -42,14 +29,16 @@ class OBSEPackaging_Conan(ConanFile):
         #---RecommendedIntegration
         self.copy("RecommendedIntegration.py")
         #---common
+        VS.SetIncludeDir('Oblivion-Script-Extender/common/common.vcxproj',"$(ProjectDir);$(ProjectDir)\..;%(AdditionalIncludeDirectories)")
         self.copy('common.vcxproj',src='Oblivion-Script-Extender/common/',dst='include/common/')
         self.copy('*.h',src='Oblivion-Script-Extender/common/',dst='include/common/')
         self.copy('*.cpp',src='Oblivion-Script-Extender/common/',dst='include/common/')
         self.copy('*.inl',src='Oblivion-Script-Extender/common/',dst='include/common/')
+        self.copy('*.inc',src='Oblivion-Script-Extender/common/',dst='include/common/')
         #---obse
         self.copy('*.h',src='Oblivion-Script-Extender/obse/',dst='include/obse/')
         self.copy('*.cpp',src='Oblivion-Script-Extender/obse/',dst='include/obse/')
         self.copy('*.inl',src='Oblivion-Script-Extender/obse/',dst='include/obse/')
 
     def package_info(self):
-        self.cpp_info.includedirs = ['include','include/obse']
+        self.cpp_info.includedirs = ['include','include/obse','include/common'] #'include/common' may not be necessary
